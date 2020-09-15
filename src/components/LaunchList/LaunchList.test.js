000c6209d0a16d236f0b376234eaaa64a467538c
prakhar-shukla <prakharshuklaofficial@gmail.com>
2020-09-15 19:59:26 +0530

    Test case updated

--- src/components/LaunchList/LaunchList.test.js
+++ src/components/LaunchList/LaunchList.test.js
@@ -10,6 +10,27 @@ configure({ adapter: new Adapter() });
 const launchData = new Array(10).fill().map((_, i) => { return { flight_number: i } });
 
 describe('<LaunchList/>', () => {
+    beforeEach(() => {
+        global.IntersectionObserver = class IntersectionObserver {
+            constructor() { }
+
+            disconnect() {
+                return null;
+            }
+
+            observe() {
+                return null;
+            }
+
+            takeRecords() {
+                return null;
+            }
+
+            unobserve() {
+                return null;
+            }
+        };
+    })
     it(`should render ${launchData.length} <LaunchItem/> elements`, () => {
         const wrapper = shallow(<LaunchList launchData={launchData} />);
         expect(wrapper.find(LaunchItem)).toHaveLength(launchData.length);
